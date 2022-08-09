4c1f6285e7f8a3c3fe509c4d0d5a8245aa779aa2
devashishsharma5821 <devashish.sharma@technogetic.com>
2022-08-09 12:01:11 +0530

    some styling and Icons where added

--- src/components/Layout/footer.js
+++ src/components/Layout/footer.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import classes from './Footer.module.css';
-
+import React from "react";
+import classes from "./Footer.module.css";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { pink } from "@mui/material/colors";
 export default function footer() {
   return (
     <div className={classes.footer}>
-        <p>Copyright © 2022 FoodOrderApp.com | All Rights Reserved.</p>
+      <p>Copyright © 2022 FoodOrderApp.com | All Rights Reserved.</p>
+      <div>
+        <ul className={classes.Icons}>
+          <li>
+            <FacebookIcon color="primary" fontSize="large" />
+          </li>
+          <li>
+            <InstagramIcon sx={{ color: pink[500] }} fontSize="large" />
+          </li>
+          <li>
+            <TwitterIcon color="primary" fontSize="large" />
+          </li>
+          <li>
+            <GitHubIcon fontSize="large" />
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
